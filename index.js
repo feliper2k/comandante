0d0b781d1ff35adef5aded7fae9ae3d4d1b48967
feliper <filip.m.ryba@gmail.com>
2016-02-02 13:53:02 +0100

    stderr now behaves as a normal stream

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ module.exports = function (cmd, args, opts) {
         args = cmd.slice(1);
         cmd = cmd[0];
     }
-    
+
     var ps = spawn(cmd, args, opts);
     var err = '';
     if (ps.stderr) {
         ps.stderr.on('data', function (buf) { err += buf });
     }
-    
+
     ps.on('close', function (code) {
         if (code === 0) return;
         dup.emit('error', new Error(
@@ -24,18 +24,19 @@ module.exports = function (cmd, args, opts) {
             )
             + '\n\n  ' + err));
     });
-    
+
     var dup = duplexer(ps.stdin, ps.stdout);
-    
+
     dup.stdin = ps.stdin;
     dup.stderr = ps.stderr;
     dup.stdout = ps.stdout;
     dup.pid = ps.pid;
     dup.kill = ps.kill.bind(ps);
-    
+    dup.errorOutput = err;
+
     [ 'exit', 'close' ].forEach(function (name) {
         ps.on(name, dup.emit.bind(dup, name));
     });
-    
+
     return dup;
 };
